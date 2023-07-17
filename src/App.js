d810eef481f1eaa5cb81c413085795cf71fb6959
duongni <133175915+duongni@users.noreply.github.com>
2023-07-16 21:22:23 -0400

    created Cafecard component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import Main from "./Main";
+import Cafecard from "./Cafecard";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Cafecard />
       <Footer />
     </div>
   );
---
+++ src/Cafecard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Cafecard() {
+  return (
+    <div className="Cafecard">
+      <div className="card1"></div>
+    </div>
+  );
+}
